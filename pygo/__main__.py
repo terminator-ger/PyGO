2bcb072646cd649797d41c640bdd899bfab154be
ml <michael.lechner.trash@t-online.de>
2022-11-11 19:33:27 +0100

    no threading main

--- pygo/__main__.py
+++ pygo/__main__.py
@@ -1,14 +1,14 @@
-from pygo.core import PyGO
+#from pygo.core import PyGO
 from pygo.ui.pygotk import PyGOTk
-import threading
+#import threading
 
 
 def run_app():
-    pygo = PyGO()
-    ui = PyGOTk(pygo)
-    t_l = threading.Thread(target=pygo.loop).start()
+    ui = PyGOTk()
     ui.run()
-    t_l.join()
+    #pygo = PyGO()
+    #t_l = threading.Thread(target=pygo.loop).start()
+    #t_l.join()
 
 if __name__ == "__main__":
     run_app()
