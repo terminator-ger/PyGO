62d459960f9d1c0da97f09d58219fc891b752822
ml <michael.lechner.trash@t-online.de>
2022-11-15 09:40:34 +0100

    removed double run of main

--- pygo/__main__.py
+++ pygo/__main__.py
@@ -1,10 +1,8 @@
-#from pygo.core import PyGO
 from pygo.ui.pygotk import PyGOTk
-#import threading
-
 
 def run_app():
     ui = PyGOTk()
     ui.run()
 
-run_app()
+if __name__ == '__main__':
+    run_app()
